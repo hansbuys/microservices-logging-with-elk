e6aa652788469157da756c76c72e6e793e2476c1
Hans Buys <buyshans@gmail.com>
2019-01-04 21:15:53 -0500

    Ensure uniqueness of request-id header.

--- validation/echo/test/server.test.ts
+++ validation/echo/test/server.test.ts
@@ -1,4 +1,4 @@
-import {expect} from "chai";
+import { expect } from "chai";
 import { IncomingMessage } from "http";
 import {} from "mocha";
 import "reflect-metadata";
@@ -23,17 +23,31 @@ describe("echo-validation", () => {
         server.stop();
     });
 
-    it("should be available", async () => {
+    it("should say hello", async () => {
         const result = await request.get(url);
 
         expect(result).to.equal("hello");
     });
 
-    it("should have request-id", async () => {
+    it("should have request-id as a guid", async () => {
         const result: IncomingMessage = await request.get(url, {
             resolveWithFullResponse: true,
         });
 
-        expect(result.rawHeaders).to.include("X-Request-Id");
+        expect(result.headers["x-request-id"]).to.match(/^[0-9A-Fa-f]{8}(?:-[0-9A-Fa-f]{4}){3}-[0-9A-Fa-f]{12}$/);
+    });
+
+    it("should have unique request-id", async () => {
+        const result1: IncomingMessage = await request.get(url, {
+            resolveWithFullResponse: true,
+        });
+        const result2: IncomingMessage = await request.get(url, {
+            resolveWithFullResponse: true,
+        });
+
+        const header1 = result1.headers["x-request-id"];
+        const header2 = result2.headers["x-request-id"];
+
+        expect(header1).to.not.eql(header2);
     });
 });
