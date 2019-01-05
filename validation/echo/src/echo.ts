e0a3744043a2437366b221ef4d36a2971c6f051c
Hans Buys <buyshans@gmail.com>
2019-01-04 20:35:49 -0500

    Use log instance from request.

--- validation/echo/src/echo.ts
+++ validation/echo/src/echo.ts
@@ -1,20 +1,12 @@
-import * as Logger from "bunyan";
-import { inject } from "inversify";
+import { Request } from "express";
 import { controller, httpGet } from "inversify-express-utils";
-import { LogFactory } from "./logfactory";
 
 @controller("/")
 export class EchoController {
 
-    private readonly log: Logger;
-
-    constructor(@inject(LogFactory) log: LogFactory) {
-        this.log = log.createLog(EchoController);
-     }
-
     @httpGet("/")
-    public async index(): Promise<string> {
-        this.log.debug("Saying 'hello'.");
+    public async index(req: Request): Promise<string> {
+        req.log.debug("Saying 'hello'.");
         return "hello";
     }
 }
