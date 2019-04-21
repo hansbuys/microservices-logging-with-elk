35bfa03c60542ba9331e6b708497353f0b2a53ca
Hans Buys <buyshans@gmail.com>
2019-04-20 22:06:31 -0400

    Log to file for both services.

--- delivery/src/index.ts
+++ delivery/src/index.ts
@@ -3,6 +3,6 @@ import { LogFactory } from "./logfactory";
 import { DeliveryServer } from "./server";
 
 const port: number = Number(process.env.PORT || 3000);
-const logFactory: LogFactory = new LogFactory(false);
+const logFactory: LogFactory = new LogFactory();
 
 new DeliveryServer(port, logFactory).start();
--- intake/src/index.ts
+++ intake/src/index.ts
@@ -3,6 +3,6 @@ import { LogFactory } from "./logfactory";
 import { IntakeServer as IntakeServer } from "./server";
 
 const port: number = Number(process.env.PORT || 3000);
-const logFactory: LogFactory = new LogFactory(false);
+const logFactory: LogFactory = new LogFactory();
 
 new IntakeServer(port, logFactory).start();
